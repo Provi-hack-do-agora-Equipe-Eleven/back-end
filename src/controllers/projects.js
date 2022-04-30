1e386f07af79fa6aed21e9cf5d16b58a62c02d74
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 04:00:24 -0300

    feat: create deleteProject endpoint

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -67,4 +67,24 @@ const updateProject = async (req, res) => {
   }
 };
 
-module.exports = { getProjects, createProject, updateProject };
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const addressDeleted = await knex('adresses').where({ project_id: id }).del();
+    if(!addressDeleted) {
+      return res.status(404).json({ error: 'Endereço não pode ser deletado' });
+    }
+
+    const projectDeleted = await knex('projects').where({ id }).del();
+    if(!projectDeleted) {
+      return res.status(404).json({ error: 'Projeto não pode ser deletado' });
+    }
+
+    return res.status(200).json({ message: 'Projeto deletado com sucesso' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { getProjects, createProject, updateProject, deleteProject };
