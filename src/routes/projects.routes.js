e2c4e15b1de45ab89df993e98054eb8c2c14f6de
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 19:25:35 -0300

    fix: add validateLogin for required validation routes

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -8,8 +8,10 @@ const deleteProject = require('../controllers/projectControllers/deleteProject')
 const verifyFields = require('../middlewares/projectMiddlewares/verifyFields');
 const verifyIfEmailExists = require('../middlewares/projectMiddlewares/verifyIfEmailExists');
 const verifyIfProjectExists = require('../middlewares/projectMiddlewares/verifyIfProjectExists');
+const validateLogin = require('../middlewares/validateLogin');
 
 projectRoutes.get('/projects', getProjects);
+projectRoutes.use(validateLogin);
 projectRoutes.post('/project', verifyFields, verifyIfEmailExists, createProject);
 projectRoutes.put('/project/:id', verifyIfProjectExists, verifyIfEmailExists, updateProject);
 projectRoutes.delete('/project/:id', verifyIfProjectExists, deleteProject);
