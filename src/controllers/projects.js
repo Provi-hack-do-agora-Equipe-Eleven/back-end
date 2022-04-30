e8e0bcb8a3bb09ace9daa483479acbf019ff350e
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 02:31:28 -0300

    feat: create createProject endpoint

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -7,6 +7,53 @@ const getProjects = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
-}
+};
 
-module.exports = { getProjects };
+const createProject = async (req, res) => {
+  const data = req.body;
+  const { id: user_id } = req.user;
+  
+  try {
+    const [project] = await knex('projects')
+      .insert({
+        name: data.name,
+        email: data.email,
+        owner_name: data.owner_name,
+        owner_email: data.owner_email,
+        website: data.website,
+        pix: data.pix,
+        description: data.description,
+        instagram: data.instagram,
+        created_at: data.created_at,
+        modified_at: data.modified_at,
+        project_image: data.project_image,
+        cnpj: data.cnpj,
+        user_id,
+      })
+      .returning('*');
+
+    const { city, state, street, zipcode, country, complement } = data.address;
+    await knex('adresses').insert({
+      city,
+      state,
+      street,
+      zipcode,
+      country,
+      complement,
+      project_id: project.id,
+    });
+
+    const projectCreated = await knex
+      .select('p.id', 'p.name', 'p.description', 'a.city', 'a.state', 'a.country')
+      .from('projects AS p')
+      .leftJoin('adresses AS a', 'a.project_id', 'p.id')
+      .where({ 'p.id': project.id })
+      .first();
+
+    return res.status(201).json({ project: projectCreated });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getProjects, createProject };
