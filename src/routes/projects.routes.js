3c6a5aa603009b2aec5305b1d92dad9abc7c609c
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 19:15:52 -0300

    refactor: refactoring project routes inserting into a file 'projects.routes.js'

---
+++ src/routes/projects.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const projectRoutes = Router();
+
+const getProjects = require('../controllers/projectControllers/getProjects');
+const createProject = require('../controllers/projectControllers/createProject');
+const updateProject = require('../controllers/projectControllers/updateProject');
+const deleteProject = require('../controllers/projectControllers/deleteProject');
+const verifyFields = require('../middlewares/projectMiddlewares/verifyFields');
+const verifyIfEmailExists = require('../middlewares/projectMiddlewares/verifyIfEmailExists');
+const verifyIfProjectExists = require('../middlewares/projectMiddlewares/verifyIfProjectExists');
+
+projectRoutes.get('/projects', getProjects);
+projectRoutes.post('/project', verifyFields, verifyIfEmailExists, createProject);
+projectRoutes.put('/project/:id', verifyIfProjectExists, verifyIfEmailExists, updateProject);
+projectRoutes.delete('/project/:id', verifyIfProjectExists, deleteProject);
+
+module.exports = projectRoutes;
