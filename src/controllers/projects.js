dccd03339173cee3052141846f704f8340e93731
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 12:57:08 -0300

    fix: changed getProjects function returns

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -2,7 +2,7 @@ const knex = require('../database/connection');
 
 const getProjects = async (req, res) => {
   try {
-    const projects = await knex('projects');
+    const projects = await knex.select('id', 'name', 'description').from('projects');
     return res.status(200).json({ projects: projects });
   } catch (error) {
     return res.status(500).json({ error: error.message });
