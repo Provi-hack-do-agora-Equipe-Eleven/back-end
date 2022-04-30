f06d45e49ee94c72c2fdabf736f13bd8111edb9e
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-29 21:38:51 -0300

    feat: create getProjects endpoint

---
+++ src/controllers/projects.js
@@ -0,0 +1,12 @@
+const knex = require('../database/connection');
+
+const getProjects = async (req, res) => {
+  try {
+    const projects = await knex('projects');
+    return res.status(200).json({ projects: projects });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { getProjects };
