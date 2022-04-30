8747d421a0d091153079571ec6c832058753ee00
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 03:52:31 -0300

    refactor: refactoring updateProject endpoint

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -12,19 +12,19 @@ const getProjects = async (req, res) => {
 const createProject = async (req, res) => {
   const { address, ...project } = req.body;
   project.user_id = req.user.id;
-  
+
   try {
-    const [ projectCreated ] = await knex('projects').insert(project).returning('*');
-    if(!projectCreated){
+    const [projectCreated] = await knex('projects').insert(project).returning('*');
+    if (!projectCreated) {
       return res.status(404).json({ error: 'Projeto não pode ser criado' });
-    } 
-      
+    }
+
     address.project_id = projectCreated.id;
-    
-    const [ addressCreated ] = await knex('adresses').insert(address).returning('*');
-    if(!addressCreated){
+
+    const [addressCreated] = await knex('adresses').insert(address).returning('*');
+    if (!addressCreated) {
       return res.status(404).json({ error: 'Projeto não pode ser criado' });
-    } 
+    }
 
     const { id, project_id, ...projectAddress } = addressCreated;
     projectCreated.address = projectAddress;
@@ -38,20 +38,28 @@ const createProject = async (req, res) => {
 const updateProject = async (req, res) => {
   const { address, ...project } = req.body;
   const { id } = req.params;
-  
-  try {
-    const updatedProject = await knex('projects').where({ id }).update(project);
-    if(!updatedProject) return res.status(404).json({ error: 'Projeto não pode ser atualizado' });
-    
-    const updatedAddress = await knex('adresses').where({ project_id: id }).update(address);
-    if(!updatedAddress) return res.status(404).json({ error: 'Endereço não pode ser atualizado' });
 
-    const projectUpdated = await knex('projects')
-      .select('p.id', 'p.name', 'p.description', 'a.city', 'a.state', 'a.country')
-      .from('projects AS p')
-      .leftJoin('adresses AS a', 'a.project_id', 'p.id')
-      .where({ 'p.id': id })
-      .first();
+  try {
+    const [ projectUpdated ] = await knex('projects')
+      .where({ id })
+      .update(project)
+      .returning('*');
+
+    if (!projectUpdated) {
+      return res.status(404).json({ error: 'Projeto não pode ser atualizado' });
+    }
+
+    const [ addressUpdated ] = await knex('adresses')
+      .where({ project_id: id })
+      .update(address)
+      .returning('*');
+
+    if (!addressUpdated) {
+      return res.status(404).json({ error: 'Endereço não pode ser atualizado' });
+    }
+
+    const { id: addressID, project_id, ...projectAddress } = addressUpdated;
+    projectUpdated.address = projectAddress;
 
     return res.status(200).json({ project: projectUpdated });
   } catch (error) {
