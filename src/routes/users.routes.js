fbde9b6da778147d1f6bf5812f36ee86f7041c96
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-04-30 22:04:10 -0300

    feat: create users routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,9 +5,11 @@ const getUserProjects = require('../controllers/userControllers/getUserProjects'
 const login = require('../controllers/userControllers/login');
 const signup = require('../controllers/userControllers/signup');
 const validateLogin = require('../middlewares/validateLogin');
+const verifyFields = require('../middlewares/userMiddlewares/verifyFields');
+const verifyLogin = require('../middlewares/userMiddlewares/verifyLogin');
 
-userRoutes.post('/signup', signup);
-userRoutes.post('/login', login);
+userRoutes.post('/signup', verifyFields, signup);
+userRoutes.post('/login', verifyLogin, login);
 userRoutes.use(validateLogin);
 userRoutes.get('/projects', getUserProjects);
 
