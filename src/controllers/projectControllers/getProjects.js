fc6e3ca77b77ba95738366fbe83b2b299264fdf7
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-01 18:46:28 -0300

    fix: changed getProjects function returns

--- src/controllers/projectControllers/getProjects.js
+++ src/controllers/projectControllers/getProjects.js
@@ -3,8 +3,10 @@ const knex = require('../../database/connection');
 const getProjects = async (req, res) => {
   try {
     const projects = await knex
-      .select('id', 'name', 'description')
-      .from('projects');
+      .select('p.id', 'p.name', 'p.description','p.project_image', 'a.city', 'a.state')
+      .from('projects as p')
+      .leftJoin('adresses as a', 'a.project_id', 'p.id');
+
     return res.status(200).json({ projects: projects });
   } catch (error) {
     return res.status(500).json({ error: error.message });
