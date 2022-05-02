250e0fd150e4e85596dbd06e350ae095edf810a4
DevThome <thomedvlp@gmail.com>
2022-05-01 21:20:45 -0300

    DatabaseUrl conection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,12 @@
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DATABASE_URL
+  // {
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  // },
 });
 
 module.exports = knex;
