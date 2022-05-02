8518fbf5780fd03301f7c6f54cbcdaf341f9411e
DevThome <thomedvlp@gmail.com>
2022-05-01 22:00:40 -0300

    DatabaseUrl Connection ssl true 03

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,17 @@
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    connectionSring: `${process.env.DATABASE_URL}?ssl=true`,
+  connection: 
+  // {
+  //   connectionSring: `${process.env.DATABASE_URL}?ssl=true`,
+  //   ssl: { rejectUnauthorized: false }
+  // }
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false }
   }
-  // {
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  // },
 });
 
 module.exports = knex;
